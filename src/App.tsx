8283ad8cf863cc4474b75d9571efcb504093ae3a
mkizka <g.mkizka@gmail.com>
2021-03-12 15:58:44 +0900

    基本的なペイント実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import React, { useState } from "react";
+import { KonvaEventObject } from "konva/types/Node";
+import React, { useState, useRef } from "react";
+import { Layer, Stage, Text, Line } from "react-konva";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [lines, setLines] = useState<number[][]>([]);
+  const isDrawing = useRef(false);
 
-  return <button onClick={() => setCount(count + 1)}>{count}</button>;
-}
+  const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage()!.getPointerPosition()!;
+    setLines([...lines, [pos.x, pos.y]]);
+  };
+
+  const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage()!;
+    const pos = stage.getPointerPosition()!;
+    const latestLine = lines.pop()!;
+    setLines([...lines, [...latestLine, pos.x, pos.y]]);
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+
+  return (
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMousemove={handleMouseMove}
+      onMouseup={handleMouseUp}
+    >
+      <Layer>
+        {lines.map((line, i) => (
+          <Line key={i} points={line} stroke="red" />
+        ))}
+      </Layer>
+    </Stage>
+  );
+};
 
 export default App;
