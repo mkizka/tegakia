a4ecdf43894cd66a9c16ae540c3e23c1dcdb28a1
mkizka <g.mkizka@gmail.com>
2021-03-13 01:02:08 +0900

    アニメツール基礎作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,60 @@
 import { KonvaEventObject } from "konva/types/Node";
 import React, { useState, useRef } from "react";
-import { Layer, Stage, Text, Line } from "react-konva";
+import { Layer, Stage, Line } from "react-konva";
+
+type Line = number[];
+
+type Page = {
+  lines: Line[];
+  redoableLines: Line[];
+};
+
+function splitArrayIntoThree<T>(index: number, array: T[]): [T[], T, T[]] {
+  const prevArray = index > 0 ? [...array].slice(0, index) : [];
+  const [item, ...nextArray] = [...array].splice(index);
+  return [prevArray, item, nextArray];
+}
+
+function usePages() {
+  const [pages, setPages] = useState<Page[]>([
+    { lines: [], redoableLines: [] },
+  ]);
+  const [pageIndex, setPageIndex] = useState(0);
+  const [prevPages, currentPage, nextPages] = splitArrayIntoThree(
+    pageIndex,
+    pages
+  );
+  function setCurrentPage(newPage: Page) {
+    setPages([...prevPages, newPage, ...nextPages]);
+  }
+  return {
+    pageIndex,
+    setPageIndex,
+    currentPage,
+    setCurrentPage,
+    pages,
+    setPages,
+  };
+}
 
 const App = () => {
-  const [lines, setLines] = useState<number[][]>([]);
+  const {
+    pageIndex,
+    setPageIndex,
+    currentPage,
+    setCurrentPage,
+    pages,
+    setPages,
+  } = usePages();
   const isDrawing = useRef(false);
 
   const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
     isDrawing.current = true;
     const pos = e.target.getStage()!.getPointerPosition()!;
-    setLines([...lines, [pos.x, pos.y]]);
+    setCurrentPage({
+      lines: [...currentPage.lines, [pos.x, pos.y]],
+      redoableLines: currentPage.redoableLines,
+    });
   };
 
   const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
@@ -18,28 +63,53 @@ const App = () => {
     }
     const stage = e.target.getStage()!;
     const pos = stage.getPointerPosition()!;
-    const latestLine = lines.pop()!;
-    setLines([...lines, [...latestLine, pos.x, pos.y]]);
+    const currentLine = currentPage.lines.pop()!;
+    setCurrentPage({
+      lines: [...currentPage.lines, [...currentLine, pos.x, pos.y]],
+      redoableLines: currentPage.redoableLines,
+    });
   };
 
   const handleMouseUp = () => {
     isDrawing.current = false;
   };
 
+  const backPage = () => {
+    if (pageIndex > 0) {
+      setPageIndex(pageIndex - 1);
+    }
+  };
+
+  const pushPage = () => {
+    if (pageIndex == pages.length - 1) {
+      setPages([...pages, { lines: [], redoableLines: [] }]);
+    }
+    setPageIndex(pageIndex + 1);
+  };
+
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={handleMouseDown}
-      onMousemove={handleMouseMove}
-      onMouseup={handleMouseUp}
-    >
-      <Layer>
-        {lines.map((line, i) => (
-          <Line key={i} points={line} stroke="red" />
-        ))}
-      </Layer>
-    </Stage>
+    <>
+      <p>
+        <button onClick={backPage}>{"<"}</button>
+        <button onClick={pushPage}>{">"}</button>
+      </p>
+      <p>
+        {pageIndex + 1}/{pages.length}
+      </p>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
+      >
+        <Layer>
+          {currentPage.lines.map((line, i) => (
+            <Line key={i} points={line} stroke="red" />
+          ))}
+        </Layer>
+      </Stage>
+    </>
   );
 };
 
