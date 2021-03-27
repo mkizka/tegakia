f91a78304a491486cd326e008f8e5a54e5951bc4
mkizka <g.mkizka@gmail.com>
2021-03-27 22:11:44 +0900

    画像の再生成

--- cypress/integration/canvas.ts
+++ cypress/integration/canvas.ts
@@ -11,7 +11,7 @@ describe("ペン", () => {
       .trigger("mouseup")
       .toMatchImageSnapshot({
         imageConfig: {
-          threshold: 0.01,
+          threshold: 0.001,
         },
         name: "mouse-simple-draw",
       });
