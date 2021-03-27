bf4a3c7ef05c623667c890a7ab617488aea6f4f2
mkizka <g.mkizka@gmail.com>
2021-03-27 22:11:24 +0900

    グローバルなCSSの移動とフォント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React, { useRef } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, GlobalStyle } from "@chakra-ui/react";
 
 import { emptyPage } from "./pages";
 import { useNote } from "./useNote";
 import Menu from "./Menu";
 import Canvas from "./Canvas";
+import { Global } from "@emotion/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: `"M PLUS 1p", sans-serif`,
+  },
+  styles: {
+    global: {
+      overscrollBehavior: "none",
+    },
+  },
+});
+
+const styles = `
+  @import url("https://fonts.googleapis.com/css2?family=M+PLUS+1p");
+`;
 
 const App = () => {
   const note = useNote([emptyPage()]);
   const isDrawing = useRef(false);
 
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider theme={theme} resetCSS>
+      <Global styles={styles} />
       <Menu note={note} onPageChanged={() => (isDrawing.current = false)} />
       <Canvas note={note} isDrawing={isDrawing} />
     </ChakraProvider>
