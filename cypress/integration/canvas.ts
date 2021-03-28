40d2470ffa1aca4c92e601b0ca5f3b1d09e8a399
mkizka <g.mkizka@gmail.com>
2021-03-28 20:36:28 +0900

    failureThreshold追加

--- cypress/integration/canvas.ts
+++ cypress/integration/canvas.ts
@@ -9,6 +9,8 @@ describe("ペン", () => {
       .trigger("mousemove", { x: 70, y: 220 })
       .trigger("mousemove", { x: 100, y: 220 })
       .trigger("mouseup")
-      .matchImageSnapshot("mouse-simple-draw");
+      .matchImageSnapshot("mouse-simple-draw", {
+        failureThreshold: 0.01,
+      });
   });
 });
